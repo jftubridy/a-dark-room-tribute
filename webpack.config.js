29b85a637e5755b97c1f13744a878c8fce8e1bc3
Joseph Tubridy <jftubridy@gmail.com>
2020-03-10 18:55:29 -0700

    real depressed

--- webpack.config.js
+++ webpack.config.js
@@ -75,7 +75,7 @@ module.exports = {
      new HtmlWebpackPlugin({
        template:'template.ejs',
        appMountId: 'react-app-root',
-       title: 'React Help Queue',
+       title: 'Recreate A Dark Room',
        filename: resolve(__dirname, "build", "index.html"),
      }),
   ]
